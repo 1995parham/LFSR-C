db2e7da8ada21cc6043afd2c6daf16d3f818ccfe
Parham Alvani <parham.alvani@gmail.com>
2016-03-07 08:09:32 +0330

    LFSR.c was reformatted.

--- lfsr.c
+++ lfsr.c
@@ -42,14 +42,14 @@
  */
 #include "lfsr.h"
 
-void GLFSR_init(lfsr_t *glfsr, lfsr_data_t polynom, lfsr_data_t seed_value)
+void GLFSR_new(lfsr_t *glfsr, lfsr_data_t polynom, lfsr_data_t seed_value)
 {
 	lfsr_data_t seed_mask;
 	unsigned int shift = 8 * sizeof(lfsr_data_t) - 1;
-	
+
 	glfsr->polynomial = polynom | 1;
 	glfsr->data = seed_value;
-	
+
 	seed_mask = 1;
 	seed_mask <<= shift;
 
@@ -60,19 +60,18 @@ void GLFSR_init(lfsr_t *glfsr, lfsr_data_t polynom, lfsr_data_t seed_value)
 		}
 		seed_mask >>= 1;
 	}
-	return;
 }
 
 unsigned char GLFSR_next(lfsr_t *glfsr)
 {
 	unsigned char retval = 0;
-	
+
 	glfsr->data <<= 1;
-	
+
 	if (glfsr->data & glfsr->mask) {
 		retval = 1;
 		glfsr->data ^= glfsr->polynomial;
 	}
-	
-	return retval ;
+
+	return retval;
 }
