06cbfafcc38d853a2aa971703053fe263f295aa4
Parham Alvani <parham.alvani@gmail.com>
2016-05-18 05:26:44 +0430

    Creating LFSR8 interface

---
+++ lfsr.go
@@ -0,0 +1,27 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 18-05-2016
+ * |
+ * | File Name:     lfsr.go
+ * +===============================================
+ */
+package lfsr
+
+type LFSR8 interface {
+	Init(uint8 poly, uint8 seed)
+	Next() uint8
+}
+
+type glfsr8 struct {
+	data uint8
+	seed uint8
+	poly uint8
+}
+
+func (g *glfsr8) Init(uint8 poly, uint8 seed) {
+}
+
+func (g *glfsr8) Next() uint8 {
+}
