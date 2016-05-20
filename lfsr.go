75a2e57d9539034a634b0798227fe2f29f4504b4
Parham Alvani <parham.alvani@gmail.com>
2016-05-20 21:01:06 +0430

    Add new lfsr interfaces.

--- lfsr.go
+++ lfsr.go
@@ -13,3 +13,18 @@ type LFSR8 interface {
 	Init(poly uint8, seed uint8)
 	Next() uint8
 }
+
+type LFSR16 interface {
+	Init(poly uint16, seed uint16)
+	Next() uint16
+}
+
+type LFSR32 interface {
+	Init(poly uint32, seed uint32)
+	Next() uint32
+}
+
+type LFSR64 interface {
+	Init(poly uint64, seed uint64)
+	Next() uint64
+}
