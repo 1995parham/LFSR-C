5155ca2af462d7dd9f85e31b13e084d62830d9c0
Parham Alvani <parham.alvani@gmail.com>
2018-08-05 09:00:10 +0430

    Better comments.

--- lfsr/lfsr.go
+++ lfsr/lfsr.go
@@ -10,25 +10,25 @@
 
 package lfsr
 
-// LFSR8 Linear Feedback Shift Register 8bit
+// LFSR8 8-bit Linear Feedback Shift Register
 type LFSR8 interface {
 	Init(poly uint8, seed uint8)
 	Next() uint8
 }
 
-// LFSR16 Linear Feedback Shift Register 16bit
+// LFSR16 16-bit Linear Feedback Shift Register
 type LFSR16 interface {
 	Init(poly uint16, seed uint16)
 	Next() uint16
 }
 
-// LFSR32 Linear Feedback Shift Register 32bit
+// LFSR32 32-bit Linear Feedback Shift Register
 type LFSR32 interface {
 	Init(poly uint32, seed uint32)
 	Next() uint32
 }
 
-// LFSR64 Linear Feedback Shift Register 64bit
+// LFSR64 64-bit Linear Feedback Shift Register
 type LFSR64 interface {
 	Init(poly uint64, seed uint64)
 	Next() uint64
